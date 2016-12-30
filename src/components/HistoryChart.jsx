48b151552cb49985d50efbd41f1ef4bc3bfa7f51
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2016-12-30 12:54:58 +0800

    1.2.0
    add history charts

--- src/components/HistoryChart.jsx
+++ src/components/HistoryChart.jsx
@@ -28,14 +28,17 @@ export default class HistoryChart extends React.Component {
   }
   render() {
     return (
-      <LineChart width={ 480 } height={ 270 } data={ this.state.timeline }
-        margin={{ top: 0, right: 0, left: 0, bottom: 0 }}>
-        <XAxis dataKey="Timestamp" />
-        <YAxis dataKey="Price" />
+      <LineChart
+        width={ 480 } height={ 270 }
+        data={ this.state.timeline }
+        margin={{ top: 0, right: 0, left: 0, bottom: 0 }}
+      >
+        <XAxis dataKey="Timestamp" name="日期" />
+        <YAxis dataKey="Price" name="价格(元)" />
         <CartesianGrid strokeDasharray="3 3" />
         <Tooltip />
         <Legend />
-        <Line dataKey="Price" />
+        <Line dataKey="Price" name="价格" unit="元" dot={ false }/>
       </LineChart>
     )
   }
